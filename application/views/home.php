9a6b3429a1da9cb50c3e18719159eb7f336e0d81
shaik-subhani <subhani.sk@thatonepx.com>
2017-08-04 18:01:42 +0530

    completed  form validation

--- application/views/home.php
+++ application/views/home.php
@@ -1,29 +1,29 @@
 <?=$Header;?>
-	<div class="login-wrap">
-	<div class="login-html">
-	 <form class="memory form-horizontal" role="form" id="ToneRegisterForm" action="home/register" method="POST" autocomplete="false">
-		<div class="login-form">
-		<?php
+     <div class="login-wrap">
+    <div class="login-html" >
+     <form class="memory" role="form" id="signup-form" action="home/register" method="POST">
+	         <?php
          			print_r($this->session->flashdata('Errors'));
-       	?>
-			<div class="sign-in-htm">
-				<div class="group" >
-					<label for="user" class="label">FirstName :</label>
-					<input name="firstname" type="text" class="input" maxlength="20" minlength="2"  autocomplete="false" />
-				</div>
-				<div class="group">
-					<label for="pass" class="label" >LastName :</label>
-					<input name="lastname" type="text" class="input"  maxlength="20" minlength="2" autocomplete="false" />
-				</div>
-				<div class="group">
-					<label for="pass" class="label" id="ageinyears">Age In Years:</label>
-					<input type="text" class="input" maxlength="3" name="age" autocomplete="false" >
-				</div>
-				<div class="form-group">
-                    <label class="control-label col-sm-4">Gender : </label>
+         		?>
+        <div class="login-form">
+            <div class="sign-in-htm">
+                <div class="group">
+                    <label for="pass" id="first" class="label">FirstName :</label>
+                    <input name="firstname" id="user" type="text" class="input" />
+                </div>
+                <div class="group">
+                    <label for="pass" id="last" class="label" >LastName :</label>
+                    <input name="lastname" id="name" type="text" class="input" >
+                </div>
+                <div class="group">
+                    <label for="pass" label="years" class="label" id="ageinyears">Age In Years :</label>
+                    <input type="text" id="years" class="input" >
+                </div>
+                <div class="form-group">
+                    <label class="label col-sm-4">GENDER : </label>
                     <input type="hidden" name="gender" id="hdnGender">  
-                    <div class="col-sm-6">
-                        <div class="row" style="display: inline-flex; width: 100%; padding-top:12px; margin-bottom: 50px;">
+                    <div class="col-sm-6" >
+                        <div class="row" style="display: inline-flex; width: 100%; padding-top:12px; margin-bottom: 50px;"><br>
                              <div class="col-sm-5">
                                 <label class="radio-inline">
                                     <input type="radio" id="rdMaleGender" value="Male" name="sex" onclick="$('#hdnGender').val($(this).val());">Male
@@ -37,50 +37,101 @@
                         </div>
                     </div>
                 </div>
-				<div class="form-group" style="display:none;">
-                    <label for="sleFileNumber" class="col-sm-4 control-label" >File Number :</label>
-                    <div class="col-sm-8">
-                         <input type="text" id="sleFileNumber" placeholder="File Number" class="form-control" name="fileNumber" value="103B-D-2017-" autocomplete="false" />
-                    </div>
+                
+                <div class="group" align="center">
+                    <label for="pass" label="years" class="label" id="register"></label>
+                    <input type="submit" value="Register" id="register"  style="width:40vh; height:7vh; background-color:blue; border-radius:20px; color:white;">
                 </div>
-				<div class="group" id="register">
-					<label for="pass" class="label" id="register"></label>
-					<input type="submit" class="button" id="register" value="Register">
-				</div>
-				
-				
-			</div>
-		</div>
-	</form>
-</div>
-</div>
-		<!-- JS files will load here -->
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<script src="resources/js/bootstrapValidator.min.js"></script>
-		<script type="text/javascript">
-			$(document).ready(function() {
-				$('#ToneRegisterForm').bootstrapValidator({
-					message: 'This value is not valid',
-					fields: {
-						firstname: {
-							validators: {
-								notEmpty: {
-									message: 'The firstname is required and can\'t be empty'
-								},
-								// stringLength: {
-									// min: 2,
-									// max: 30,
-									// message: 'The firstname must be more than 1 and less than 30 characters long'
-								// },
-								// regexp: {
-									// regexp: /^[a-zA-Z0-9_\.]+$/,
-									// message: 'The firstname can only consist of alphabetical, number, dot and underscore'
-								// }
-							}
-						}
-					}
-				});
-			});
-		</script>
+                
+            </div>
+        </div>
+    </form>
+    </div>
+    <div class="register login-html register" id="number" style="display:none;" >
+     <form class="memory" role="form" id="signup-form">
+        <div class="login-form">
+            <div class="sign-in-htm">
+                <div class="group">
+                    <label for="pass" id="first" class="label">File Number :</label>
+                    <input name="filenumber" id="file" type="text" class="input" />
+                </div>
+                <div class="group" align="center">
+                    <label for="pass" label="years" class="label" id="register"></label>
+                    <input type="submit" value="Register" id="register"  style="width:40vh; height:7vh; background-color:blue; border-radius:20px; color:white;">
+                </div>
+                
+            </div>
+        </div>
+    </form>
+    </div>
+  </div>  
+  
+  
+<script>
+    $('.memory').on('submit',  function(e) {
+    e.preventDefault();
+    
+     var filenumberReg = /^[0-9]+$/;
+      var filenumber = $('#file').val();
+     var message='';  
+     var inputVal = new Array(filenumber,message);
+      var inputMessage = new Array("filenumber","message");
+    if(inputVal[0] == ""){
+            $('#file').after('<span class="error"> Please enter your ' + inputMessage[0] + '</span>');
+        }
+        else if(!filenumberReg.test(filenumber)){
+            $('#file').after('<span class="error"> Digits only</span>');
+            
+        }
+        if(inputVal[1] == ""){
+            $('#messageLabel').after('<span class="error"> Please enter your ' + inputMessage[1] + '</span>');
+        }   
+    
+    });
+$(document).ready(function(){
+$('#signup-form').on('submit',   function(e) {
+    e.preventDefault();
+    validateForm();
+});
+    
+function validateForm(){
+    console.log('test');
+    
+    var firstnameReg = /^[A-Za-z]+$/;
+    var lastnameReg = /^[A-Za-z]+$/;
+    var ageinyearsReg =  /^[0-9]+$/; 
+    var firstname = $('#user').val();
+    var lastname = $('#name').val();
+    var ageinyears = $('#years').val();
+    var gender    = $('#hdnGender').val();
+    var message = $('#messageInput').val();
+    var inputVal = new Array(firstname, lastname, ageinyears,  gender, message);
+    var inputMessage = new Array("firstname", "lastname", "ageinyears",  "gender", "message");
+     $('.error').hide();
+        if(inputVal[0] == ""){
+            $('#user').after('<span class="error"> Please enter your ' + inputMessage[0] + '</span>');
+        } 
+        else if(!firstnameReg.test(firstname)){
+            $('#user').after('<span class="error"> Letters only</span>');
+        }
+        if(inputVal[1] == ""){
+            $('#name').after('<span class="error"> Please enter your ' + inputMessage[1] + '</span>');
+        } 
+        else if(!lastnameReg.test(lastname)){
+            $('#name').after('<span class="error"> Letters only</span>');
+        }
+        if(inputVal[2] == ""){
+            $('#years').after('<span class="error"> Please enter your ' + inputMessage[2] + '</span>');
+        }
+        else if(!ageinyearsReg.test(ageinyears)){
+            $('#years').after('<span class="error"> Digits only</span>');
+        }
+        
+        
+        if(inputVal[4] == ""){
+            $('#messageLabel').after('<span class="error"> Please enter your ' + inputMessage[4] + '</span>');
+        }   
+    }
+});
+</script>
 <?=$Footer;?>
